33ff60f6fd1140ce7b3e185ae759447a90d0d2ec
Petr <petr.movsesjan20@gmail.com>
2025-12-29 17:37:48 +0100

    first commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE,
-  authDomain: "blog-5ad16.firebaseapp.com",
-  projectId: "blog-5ad16",
-  storageBucket: "blog-5ad16.appspot.com",
-  messagingSenderId: "774415647132",
-  appId: "1:774415647132:web:07a12d420ce934ba074859"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
